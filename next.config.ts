65236f606ab6e619aeae0ebbb2017e88b827a545
joashhsaoj <j5o6a2s7h4@outlook.com>
2025-03-28 12:52:34 +0800

    2025-03-28 B-2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: isProd
+    ? "https://cdn.jsdelivr.net/gh/joashhsaoj/Yrksed@static/out/"
+    : "",
   /* config options here */
 };
 
